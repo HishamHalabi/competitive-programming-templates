4384a74ba0a1f66ac3339f04c36a8655a2c516e7
HishamHalabi <159281419+HishamHalabi@users.noreply.github.com>
2025-06-24 03:54:21 +0300

    Create BinaryTrie.cpp

---
+++ Strings/BinaryTrie.cpp
@@ -0,0 +1,57 @@
+
+
+struct Node {
+    int sz = 0   ,  ends = 0 ;
+    int ch[2]{};
+
+    int &operator[]  (int  i) {
+        return ch[i];
+    }
+};
+
+
+struct Trie {
+    vector <Node > nodes;
+
+    int mxBits  ;
+    int newNode  ()  {
+        nodes.push_back(Node());
+        return nodes.size()-1 ;
+    }
+
+    Trie(int mxBits =  31) {
+        newNode() ;
+        this->mxBits = mxBits;
+    }
+
+    void update (int num  ,  int  op)  {
+        int idx = 0 ;
+        for   (int  i = mxBits ; i >= 0 ; i--) {
+            int bit = ((1LL << i )&num) ?1 : 0;
+            if (!nodes[idx][bit]) nodes[idx][bit] = newNode()   ;
+            idx = nodes[idx][bit]  ;
+            nodes[idx].sz+=op   ;
+        }
+        nodes[idx].ends+=op ;
+    }
+
+    int min_xor(int val)  { //  assuming there are values that u must choose one of them >>  as we walk full path
+        int idx= 0 , ret = 0   ;
+        for (int i = mxBits ; i>=0 ; --i) {
+            int bit = ((1LL << i )&val) ?1 : 0;
+            int mv = nodes[idx][bit] ;
+            if (nodes[idx][bit] and nodes[mv].sz)  {  // bit^bit
+                idx= mv;
+            }else   {
+                idx = nodes[idx][1-bit] ;
+                ret+= (1LL << i ) ;
+            }
+
+        }
+        return ret ;
+    }
+
+} ;
+
+
+
